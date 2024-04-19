9d169eba43fddb300fd9367d3df127373738b708
basamaryan <basam.aryan@gmail.com>
2024-04-20 01:49:28 +0200

    miuicamera: Extract from sweet V14.0.9.0.TKFMIXM
    Change-Id: I002a92b8b121e934b2142079fe5bd3611219f109

--- common/common-vendor.mk
+++ common/common-vendor.mk
@@ -5,3 +5,12 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/sweet-miuicamera/common
 
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmicampostproc_client \
+    vendor.xiaomi.hardware.campostproc@1.0 \
+    libarcdualcamsat \
+    libarcsat \
+    MiuiCamera
